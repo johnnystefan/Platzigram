532ca9b1efb3e6e9a4fc36a0ca3ca4826bd853e1
Johnny Ordoñez <johnny.ord.mzk@gmail.com>
2019-06-17 16:29:12 -0400

    Added django-heroku

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -158,3 +159,5 @@ MEDIA_URL = '/media/'
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = LOGIN_URL
+
+django_heroku.settings(locals())
