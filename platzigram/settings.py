452bfa33252505449c1a0872281e3a8a757c7377
Johnny Ordoñez <johnny.ord.mzk@gmail.com>
2019-06-17 17:10:26 -0400

    STORAGE

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -147,10 +147,10 @@ STATICFILES_DIRS = (
 )
 
 # config deveop
-# STATICFILES_FINDERS = [
-#     'django.contrib.staticfiles.finders.FileSystemFinder',
-#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-# ]
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
